c0762ff9e5309cfee2f9b8807daf23fafdb0fcb9
haddyadnan <haddyadnan@gmail.com>
2022-12-10 12:29:05 +0100

    Add clear route

--- app.py
+++ app.py
@@ -1,63 +1,60 @@
-#!/usr/bin/python3
-
-from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
-from fastai.vision.all import *
-from io import BytesIO
-import base64
+import os
+import urllib.request
 
 app = Flask(__name__)
 
-learn = None
-labels = None
+UPLOAD_FOLDER = "static/uploads/"
 
-allowed_ext = ["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"]
+app.secret_key = "summer"  # test
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+
+allowed_ext = set(["png", "jpg", "jpeg"])
 
 
 def check_allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_ext
 
 
-# def load_model():
-#     global learn
-#     global labels
-#     learn = load_learner("export.pkl")
-#     labels = learn.dls.vocab
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        if "file" not in request.files:
-            print("No files here")
-            return redirect(request.url)
-        file = request.files["file"]
-        if file.filename == "":
-            print("No file selected here")
-            return redirect(request.url)
-        if file and check_allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            img = Image.open(file.stream)
-            with BytesIO() as buf:
-                img.save(buf, "jpeg")
-                image_bytes = buf.getvalue()
-            encoded_string = base64.b64encode(image_bytes).decode()
-        return render_template("index.html", img_data=encoded_string), 200
+@app.route("/", methods=["POST"])
+def upload_image():
+    global filename
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        flash("No image selected for uploading")
+        return redirect(request.url)
+    if file and check_allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        # print('upload_image filename: ' + filename)
+        flash("Image successfully uploaded")
+        return render_template("index.html", filename=filename)
     else:
-        return render_template("index.html", img_data=""), 200
+        flash("Allowed image types are - png, jpg, jpeg")
+        return redirect(request.url)
 
 
-@app.route("/predict", methods=["GET", "POST"])
-def predict():
-    """
-    receive image
-    """
+@app.route("/display/<filename>")
+def display_image(filename):
+    return redirect(url_for("static", filename="uploads/" + filename))
 
-    return jsonify(message="Success")
+
+@app.route("/clear", methods=["POST"])
+def clear():
+    os.remove(UPLOAD_FOLDER + filename)
+    return redirect("/")
 
 
 if __name__ == "__main__":
-    print(" * Loading Model . . .")
-    # load_model()
-    print(" * Model Loaded . . .")
+    # test
     app.run(port="5020", host="0.0.0.0", debug=True)
