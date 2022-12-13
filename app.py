407d16a46b3b607d0d5bc60914a07d2cf4195774
haddyadnan <haddyadnan@gmail.com>
2022-12-13 10:41:35 +0100

    Add predict api

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
-from werkzeug.utils import secure_filename
+import base64
 import os
 import urllib.request
+
+import requests
+from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -15,16 +18,19 @@ allowed_ext = set(["png", "jpg", "jpeg"])
 
 
 def check_allowed_file(filename):
+    """check file extension"""
     return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_ext
 
 
 @app.route("/")
 def home():
+    """render home page"""
     return render_template("index.html")
 
 
 @app.route("/", methods=["POST"])
 def upload_image():
+    """upload image file"""
     global filename
     if "file" not in request.files:
         flash("No file part")
@@ -46,13 +52,49 @@ def upload_image():
 
 @app.route("/display/<filename>")
 def display_image(filename):
+    """
+    display img
+    filename: Name of image file
+    """
     return redirect(url_for("static", filename="uploads/" + filename))
 
 
 @app.route("/clear", methods=["POST"])
 def clear():
+    """
+    remove image file
+    global filename
+    """
     os.remove(UPLOAD_FOLDER + filename)
     return redirect("/")
+
+
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+    """
+    predict image
+    """
+    with open(UPLOAD_FOLDER + filename, "rb") as img_file:
+        img_str = base64.b64encode(img_file.read()).decode()
+
+    response = requests.post(
+        "https://haddy-catdenfy.hf.space/run/predict",
+        json={
+            "data": [
+                "data:image/png;base64,{}".format(img_str),
+            ]
+        },
+    ).json()
+
+    results = response["data"][0]
+    print(results.get("label").replace("_", " "))
+    probs = results.get("confidences")
+    probs_dict = {
+        list(p.values())[0].replace("_", " "): list(p.values())[1] for p in probs
+    }
+    # print(probs_dict)
+
+    return jsonify(probs, probs.label)
 
 
 if __name__ == "__main__":
